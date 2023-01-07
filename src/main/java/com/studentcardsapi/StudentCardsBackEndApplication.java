d80d3adab7fe9f9da6453fa1883b35aed5d9f02e
IagoGuedes <95669244+IagoAGuedes24@users.noreply.github.com>
2023-01-07 12:31:34 -0300

    Initial commit

---
+++ src/main/java/com/studentcardsapi/StudentCardsBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.studentcardsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCardsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCardsBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentcardsapi/StudentCardsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentcardsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCardsBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
