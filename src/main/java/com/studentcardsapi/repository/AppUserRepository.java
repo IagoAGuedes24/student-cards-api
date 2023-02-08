9d90d5f4bc96530e6b82aeadb20369b12b28a21a
Vinícius Azevedo <vinicius.azevedo@cobli.co>
2023-02-08 16:06:26 -0300

    Update AppUserRepository.java

--- src/main/java/com/studentcardsapi/repository/AppUserRepository.java
+++ src/main/java/com/studentcardsapi/repository/AppUserRepository.java
@@ -1,14 +1,18 @@
 package com.studentcardsapi.repository;
 
+import com.studentcardsapi.enums.AppUserRole;
 import com.studentcardsapi.model.user.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.Optional;
 
-@NoRepositoryBean
+@Repository
 public interface AppUserRepository<T extends AppUser> extends JpaRepository<T, Long> {
 
-    Optional<AppUser> findByUsername(String username);
+    Optional<T> findByUsername(String username);
+
+    Collection<AppUser> findAllByUserRole(AppUserRole userRole);
 
 }
