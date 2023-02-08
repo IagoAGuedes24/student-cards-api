84a999857d135f03e2d4a8ea3698fc25858dff47
Vinícius Azevedo <vinicius.azevedo@cobli.co>
2023-02-08 16:05:11 -0300

    inserts save and get app user in app user service

--- src/main/java/com/studentcardsapi/service/implementations/AppUserServiceImpl.java
+++ src/main/java/com/studentcardsapi/service/implementations/AppUserServiceImpl.java
@@ -1,14 +1,32 @@
 package com.studentcardsapi.service.implementations;
 
 
+import com.studentcardsapi.exception.ApiRequestException;
+import com.studentcardsapi.model.user.AppUser;
+import com.studentcardsapi.repository.AppUserRepository;
 import com.studentcardsapi.service.interfaces.AppUserService;
-import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 @AllArgsConstructor
 public class AppUserServiceImpl  implements AppUserService {
+
+    private static final String USER_NOT_FOUND = "Usuário não encontrado";
+
+    private AppUserRepository<AppUser> appUserRepository;
+
+    public AppUser saveAppUser(AppUser appUser) {
+        this.appUserRepository.save(appUser);
+        return appUser;
+    }
+
+    public AppUser getUser(String username) {
+        return this.appUserRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new ApiRequestException(USER_NOT_FOUND));
+    }
 
 }
--- src/main/java/com/studentcardsapi/service/interfaces/AppUserService.java
+++ src/main/java/com/studentcardsapi/service/interfaces/AppUserService.java
@@ -4,4 +4,7 @@ import com.studentcardsapi.model.user.AppUser;
 
 public interface AppUserService {
 
+    public AppUser getUser(String username);
+    AppUser saveAppUser(AppUser appUser);
+
 }
