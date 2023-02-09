752dcb55b2a6d08ace45185bf3fb76fdb135df1b
Vinícius Azevedo <vinicius.azevedo@cobli.co>
2023-02-09 11:19:11 -0300

    updates check cpf logic

--- src/main/java/com/studentcardsapi/service/implementations/AppUserServiceImpl.java
+++ src/main/java/com/studentcardsapi/service/implementations/AppUserServiceImpl.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.UUID;
 
 import static com.studentcardsapi.utils.BusinessRulesConstants.MINUTES_FOR_USERNAME_CONFIRMATION_EXPIRATION;
@@ -70,13 +71,12 @@ public class AppUserServiceImpl  implements AppUserService {
 
         checkUsername(userRegistrationDTO.getUsername());
         checkCpf(userRegistrationDTO.getCpf());
-        AppUser appUser = this.modelMapper.map(userRegistrationDTO, AppUser.class);
+        AppUser appUser = new AppUser();
+        appUser = this.modelMapper.map(userRegistrationDTO, AppUser.class);
         setDefaultValues(appUser);
 
         this.emailService.sendUsernameConfirmation(appUser.getUsername(), appUser.getUsernameConfirmationToken());
-        // TODO Confirmando, o usuário fica validado no sistema e a entidade é validada
         // TODO REVISAR UTILIZAÇÃO DE GETS COM BASE EM ENABLED
-        // TODO CRIAR TOKEN DE SOLICITAÇÃO DE NOVO LINK
 
         return this.saveAppUser(appUser);
     }
@@ -97,33 +97,56 @@ public class AppUserServiceImpl  implements AppUserService {
 
     private void checkCpf(String cpf) {
 
-        if (cpf.length() != 11) {
+        // considera-se erro CPF's formados por uma sequencia de numeros iguais
+        if (cpf.equals("00000000000") ||
+                cpf.equals("11111111111") ||
+                cpf.equals("22222222222") || cpf.equals("33333333333") ||
+                cpf.equals("44444444444") || cpf.equals("55555555555") ||
+                cpf.equals("66666666666") || cpf.equals("77777777777") ||
+                cpf.equals("88888888888") || cpf.equals("99999999999") ||
+                (cpf.length() != 11))
             throw new ApiRequestException(INVALID_CPF);
-        }
 
-        int[] cpfArray = new int[11];
-        for (int i = 0; i < 11; i++) {
-            cpfArray[i] = Integer.parseInt(cpf.charAt(i) + "");
-            if (i > 0 && cpfArray[i] == cpfArray[i - 1]) {
-                throw new ApiRequestException(INVALID_CPF);
+        char dig10, dig11;
+        int sm, i, r, num, peso;
+
+        // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
+        try {
+            // Calculo do 1o. Digito Verificador
+            sm = 0;
+            peso = 10;
+            for (i=0; i<9; i++) {
+                // converte o i-esimo caractere do CPF em um numero:
+                // por exemplo, transforma o caractere '0' no inteiro 0
+                // (48 eh a posicao de '0' na tabela ASCII)
+                num = (int)(cpf.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
             }
-        }
 
-        int firstDigit = 0;
-        for (int i = 0; i < 9; i++) {
-            firstDigit += (10 - i) * cpfArray[i];
-        }
-        firstDigit = (firstDigit % 11) < 2 ? 0 : 11 - (firstDigit % 11);
-        if (firstDigit != cpfArray[9]) {
-            throw new ApiRequestException(INVALID_CPF);
-        }
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11))
+                dig10 = '0';
+            else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
 
-        int secondDigit = 0;
-        for (int i = 0; i < 10; i++) {
-            secondDigit += (11 - i) * cpfArray[i];
-        }
-        secondDigit = (secondDigit % 11) < 2 ? 0 : 11 - (secondDigit % 11);
-        if (secondDigit != cpfArray[10]) {
+            // Calculo do 2o. Digito Verificador
+            sm = 0;
+            peso = 11;
+            for(i=0; i<10; i++) {
+                num = (int)(cpf.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11))
+                dig11 = '0';
+            else dig11 = (char)(r + 48);
+
+            // Verifica se os digitos calculados conferem com os digitos informados.
+            if (!(dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10)))
+                throw new ApiRequestException(INVALID_CPF);
+        } catch (InputMismatchException erro) {
             throw new ApiRequestException(INVALID_CPF);
         }
 
@@ -138,6 +161,8 @@ public class AppUserServiceImpl  implements AppUserService {
     }
 
     public void generateActivationToken(AppUser appUser) {
+        if (appUser.isEnabled())
+            throw new ApiRequestException(USER_ALREADY_ENABLED);
         appUser.setUsernameConfirmationToken(UUID.randomUUID().toString());
         appUser.setUsernameConfirmationTokenExpiration(new Date(System.currentTimeMillis() + MINUTES_FOR_USERNAME_CONFIRMATION_EXPIRATION * 60 * 1000));
     }
