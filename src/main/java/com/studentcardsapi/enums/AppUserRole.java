51d1e6b91668c1769b7d41980df8a1cc757f8279
Vinícius Azevedo <vinicius.azevedo@cobli.co>
2023-02-09 10:47:06 -0300

    Update AppUserRole.java

--- src/main/java/com/studentcardsapi/enums/AppUserRole.java
+++ src/main/java/com/studentcardsapi/enums/AppUserRole.java
@@ -1,8 +1,26 @@
 package com.studentcardsapi.enums;
 
+import com.studentcardsapi.exception.ApiRequestException;
+import com.studentcardsapi.model.user.*;
+
 public enum AppUserRole {
     ASSISTANT,
     COORDINATOR,
     PROFESSOR,
-    STUDENT
+    STUDENT;
+
+    public AppUser instantiante(AppUser appUser) {
+        switch (appUser.getUserRole()) {
+            case ASSISTANT:
+                return (Assistant) appUser;
+            case COORDINATOR:
+                return (Coordinator) appUser;
+            case PROFESSOR:
+                return (Professor) appUser;
+            case STUDENT:
+                return (Student) appUser;
+            default:
+                throw new ApiRequestException("Unrecognized model type");
+        }
+    }
 }
