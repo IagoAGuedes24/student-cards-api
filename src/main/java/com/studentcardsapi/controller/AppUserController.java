a1c3e3ce7d780953ffe90f404dfda0f906ce0d9c
Vinícius Azevedo <vinicius.azevedo@cobli.co>
2023-02-09 10:48:52 -0300

    Update AppUserController.java

--- src/main/java/com/studentcardsapi/controller/AppUserController.java
+++ src/main/java/com/studentcardsapi/controller/AppUserController.java
@@ -15,6 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static com.studentcardsapi.utils.ConstantEndpoints.USERNAME_CONFIRMATION_ENDPOINT;
+
 @RestController
 @RequestMapping(path = "/api/usuario")
 @AllArgsConstructor
@@ -33,6 +35,23 @@ public class AppUserController {
         UserDTO userDTO = this.modelMapper.map(appUser, UserDTO.class);
 
         return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping(USERNAME_CONFIRMATION_ENDPOINT + "/{activationToken}")
+    public ResponseEntity<?> confirmUsername(@PathVariable("activationToken") String activationToken) {
+        AppUser appUser = this.appUserService.confirmUsername(activationToken);
+        UserDTO userDTO = this.modelMapper.map(appUser, UserDTO.class);
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/gerar-novo-token")
+    public ResponseEntity<?> regenerateActivationToken(HttpServletRequest request) {
+        AppUser appUser = this.tokenDecoder.decodeAppUserToken(request);
+        this.appUserService.generateActivationToken(appUser);
+        this.appUserService.saveAppUser(appUser);
+        UserDTO userDTO = this.modelMapper.map(appUser, UserDTO.class);
+
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
     }
 
     @GetMapping("/{appUserId}")
