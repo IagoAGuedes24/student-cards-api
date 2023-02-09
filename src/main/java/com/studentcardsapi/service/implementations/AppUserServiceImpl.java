638fdfd7db109cb93d2be77c9433276ff78efa33
Vinícius Azevedo <vinicius.azevedo@cobli.co>
2023-02-08 23:51:12 -0300

    Implements user registration and get by id

--- src/main/java/com/studentcardsapi/service/implementations/AppUserServiceImpl.java
+++ src/main/java/com/studentcardsapi/service/implementations/AppUserServiceImpl.java
@@ -1,11 +1,16 @@
 package com.studentcardsapi.service.implementations;
 
 
+import com.studentcardsapi.DTO.UserRegistrationDTO;
 import com.studentcardsapi.exception.ApiRequestException;
 import com.studentcardsapi.model.user.AppUser;
 import com.studentcardsapi.repository.AppUserRepository;
 import com.studentcardsapi.service.interfaces.AppUserService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +23,10 @@ public class AppUserServiceImpl  implements AppUserService {
 
     private AppUserRepository<AppUser> appUserRepository;
 
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    private ModelMapper modelMapper;
+
     public AppUser saveAppUser(AppUser appUser) {
         this.appUserRepository.save(appUser);
         return appUser;
@@ -26,7 +35,22 @@ public class AppUserServiceImpl  implements AppUserService {
     public AppUser getUser(String username) {
         return this.appUserRepository
                 .findByUsername(username)
-                .orElseThrow(() -> new ApiRequestException(USER_NOT_FOUND));
+                .orElseThrow(() -> new UsernameNotFoundException(USER_NOT_FOUND));
     }
 
+    @Override
+    public AppUser getUser(Long id) {
+        return this.appUserRepository.findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException(USER_NOT_FOUND));
+    }
+
+    @Override
+    public AppUser registerUser(UserRegistrationDTO userRegistrationDTO) {
+        return null;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return getUser(username);
+    }
 }
--- src/main/java/com/studentcardsapi/service/interfaces/AppUserService.java
+++ src/main/java/com/studentcardsapi/service/interfaces/AppUserService.java
@@ -1,10 +1,15 @@
 package com.studentcardsapi.service.interfaces;
 
+import com.studentcardsapi.DTO.UserRegistrationDTO;
 import com.studentcardsapi.model.user.AppUser;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface AppUserService {
+public interface AppUserService extends UserDetailsService {
 
     public AppUser getUser(String username);
     AppUser saveAppUser(AppUser appUser);
 
+    AppUser getUser(Long id);
+
+    AppUser registerUser(UserRegistrationDTO userRegistrationDTO);
 }
