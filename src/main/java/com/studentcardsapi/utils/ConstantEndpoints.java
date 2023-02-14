ada6de5bb6a84dc80eaa63717ff02ecf08b16e5d
Vinícius Azevedo <vinicius.azevedo@cobli.co>
2023-02-14 12:15:55 -0300

    Refactorates EncpointConstants

--- src/main/java/com/studentcardsapi/utils/ConstantEndpoints.java
+++
@@ -1,7 +0,0 @@
-package com.studentcardsapi.utils;
-
-public class ConstantEndpoints {
-    public  static final String DEFAULT_DOMAIN = "localhost:8080/api";
-    public  static final String USERNAME_CONFIRMATION_ENDPOINT = "/ativar-conta";
-
-}
