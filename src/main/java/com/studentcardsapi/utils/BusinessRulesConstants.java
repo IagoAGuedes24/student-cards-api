ea41d0fd1c33858c14dd87d1145347eab2b244e2
Vinícius Azevedo <vinicius.azevedo@cobli.co>
2023-02-14 12:16:08 -0300

    Refactorates businessRulesConstants

--- src/main/java/com/studentcardsapi/utils/BusinessRulesConstants.java
+++
@@ -1,7 +0,0 @@
-package com.studentcardsapi.utils;
-
-public class BusinessRulesConstants {
-
-    public static final int MINUTES_FOR_USERNAME_CONFIRMATION_EXPIRATION = 30;
-
-}
---
+++ src/main/java/com/studentcardsapi/utils/constants/BusinessRulesConstants.java
@@ -0,0 +1,9 @@
+package com.studentcardsapi.utils.constants;
+
+public class BusinessRulesConstants {
+
+    public static final int MINUTES_FOR_USERNAME_CONFIRMATION_EXPIRATION = 30;
+
+    public static final String USERS_PATH = "com.studentcardsapi.model.user";
+
+}
