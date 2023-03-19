d9eeefc555032089d76bd40da1e6d667c436d1d3
Vinícius Azevedo <vinicius.azevedo@cobli.co>
2023-03-19 18:31:39 -0300

    Create SubjectController.java

---
+++ src/main/java/com/studentcardsapi/controller/SubjectController.java
@@ -0,0 +1,38 @@
+package com.studentcardsapi.controller;
+
+import com.studentcardsapi.DTO.SubjectDTO;
+import com.studentcardsapi.enums.Year;
+import com.studentcardsapi.model.Subject;
+import com.studentcardsapi.service.interfaces.SubjectService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static com.studentcardsapi.utils.constants.EndpointConstants.*;
+
+@RestController
+@RequestMapping(path = API + SUBJECT)
+@AllArgsConstructor
+@Slf4j
+
+public class SubjectController {
+
+    SubjectService subjectService;
+
+    @PostMapping(CREATION)
+    public ResponseEntity<?> createsSubject(@RequestBody SubjectDTO subjectDTO){
+        log.info("Starting Creation of subject " + subjectDTO.getName());
+        Subject subject = this.subjectService.createSubject(subjectDTO);
+
+        return new ResponseEntity<>(subject, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{year}" + LIST_ALL)
+    public ResponseEntity<?> listAllSubjects(@PathVariable Year year) {
+        log.info("SubjectController.listAllSubjects() is now being executed");
+        return new ResponseEntity<>(this.subjectService.listAllSubjects(year), HttpStatus.OK);
+    }
+
+}
