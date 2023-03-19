fab3b80cbe57b92b96b4ac568b4b7fed58404b59
Vinícius Azevedo <vinicius.azevedo@cobli.co>
2023-03-19 18:30:50 -0300

    Uodate SubjectServiceImpl

--- src/main/java/com/studentcardsapi/repository/SubjectRepository.java
+++ src/main/java/com/studentcardsapi/repository/SubjectRepository.java
@@ -1,14 +1,18 @@
 package com.studentcardsapi.repository;
 
+import com.studentcardsapi.DTO.SubjectDTO;
 import com.studentcardsapi.enums.Year;
 import com.studentcardsapi.model.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject,Long> {
 
     Optional<Subject> findByNameAndYear(String name, Year year);
+
+    List<Subject> findAllByYear(Year year);
 }
--- src/main/java/com/studentcardsapi/service/implementations/SubjectServiceImpl.java
+++ src/main/java/com/studentcardsapi/service/implementations/SubjectServiceImpl.java
@@ -1,6 +1,7 @@
 package com.studentcardsapi.service.implementations;
 
-import com.studentcardsapi.DTO.SubjectRegistrationDTO;
+import com.studentcardsapi.DTO.SubjectDTO;
+import com.studentcardsapi.enums.Year;
 import com.studentcardsapi.exception.ApiRequestException;
 import com.studentcardsapi.model.Subject;
 import com.studentcardsapi.repository.SubjectRepository;
@@ -9,6 +10,8 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 import static com.studentcardsapi.utils.messages.ErrorMessages.SUBJECT_ALREADY_EXISTS;
 
@@ -23,19 +26,27 @@ public class SubjectServiceImpl implements SubjectService {
     private ModelMapper modelMapper;
 
     @Override
-    public Subject createSubject(SubjectRegistrationDTO subjectRegistrationDTO) {
+    public Subject createSubject(SubjectDTO subjectDTO) {
 
         log.info("SubjectService.createSubject() is being executed");
-        if(this.subjectRepository.findByNameAndYear(subjectRegistrationDTO.getName(), subjectRegistrationDTO.getYear()).isPresent()){
+        if(this.subjectRepository.findByNameAndYear(subjectDTO.getName(), subjectDTO.getYear()).isPresent()){
             throw new ApiRequestException(SUBJECT_ALREADY_EXISTS);
         }
 
         log.info("subjectRegistrationDTO is valid");
 
-        Subject subject = this.modelMapper.map(subjectRegistrationDTO,Subject.class);
+        Subject subject = this.modelMapper.map(subjectDTO,Subject.class);
         this.subjectRepository.save(subject);
 
         return subject;
+    }
+
+    @Override
+    public List<Subject> listAllSubjects(Year year) {
+        log.info("SubjectService.listAllSubjects.listAllSubjects() is now being executed.");
+        List<Subject> subjectList = this.subjectRepository.findAllByYear(year);
+        log.info("Found " + subjectList.size() + "Subjects in the year " + year.name());
+        return subjectList;
     }
 
 
